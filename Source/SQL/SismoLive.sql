9797cb2cfaa3211d005805cb6dabe3c9901b6874
georgiyfarina01 <georgiy.farina@samtrevano.ch>
2020-02-21 09:00:50 +0100

    Cambiato in long il numero di telefono, aggiunto il prefisso davanti

--- Source/SQL/SismoLive.sql
+++ Source/SQL/SismoLive.sql
@@ -8,7 +8,7 @@ create table Utente(
 	nome_utente varchar(20) primary key not null,
 	psw varchar(100) not null,
 	email varchar(50) not null,
-	telefono int not null,
+	telefono long not null,
 	tipo varchar(20) not null
 );
 
@@ -39,23 +39,40 @@ create table Terremoto(
 
 # CREAZIONE AMMINISTRATORI DI BASE -----------------------------------------------------------------------------------------------
 
-insert into Utente(nome_utente,psw,email,telefono,tipo) values ("Georgiy",aes_encrypt("PasswordDiGeorgiy1","&F)H@McQfTjWnZr4"),"georgiy.farina@samtrevano.ch",0790123456,"A");
-insert into Utente(nome_utente,psw,email,telefono,tipo) values ("Marco",aes_encrypt("PasswordDiMarco","&F)H@McQfTjWnZr4"),"marco.lorusso@samtrevano.ch",0791234567,"A");
-insert into Utente(nome_utente,psw,email,telefono,tipo) values ("Matthias",aes_encrypt("PasswordDiMatthias","&F)H@McQfTjWnZr4"),"matthias.iannarella@samtrevano.ch", 0792345678,"A");
-insert into Utente(nome_utente,psw,email,telefono,tipo) values ("Daniel",aes_encrypt("PasswordDiDaniel","&F)H@McQfTjWnZr4"),"daniel.matt@samtrevano.ch",0793456789,"A");
+insert into Utente(nome_utente,psw,email,telefono,tipo) values ("Georgiy",aes_encrypt("PasswordDiGeorgiy1","&F)H@McQfTjWnZr4"),"georgiy.farina@samtrevano.ch",41790123456,"A");
+insert into Utente(nome_utente,psw,email,telefono,tipo) values ("Marco",aes_encrypt("PasswordDiMarco","&F)H@McQfTjWnZr4"),"marco.lorusso@samtrevano.ch",41791234567,"A");
+insert into Utente(nome_utente,psw,email,telefono,tipo) values ("Matthias",aes_encrypt("PasswordDiMatthias","&F)H@McQfTjWnZr4"),"matthias.iannarella@samtrevano.ch", 41792345678,"A");
+insert into Utente(nome_utente,psw,email,telefono,tipo) values ("Daniel",aes_encrypt("PasswordDiDaniel","&F)H@McQfTjWnZr4"),"daniel.matt@samtrevano.ch",41793456789,"A");
 
 # TRIGGER PER L'ALLARME TERREMOTO---------------------------------------------------------------------------------------------------------------------------------
 drop trigger if exists after_terremoto_insert;
-
+/*
 delimiter //
 create DEFINER = CURRENT_USER trigger after_terremoto_insert
 after insert on Terremoto
 for each row
-begin
-	### da inserire istruzioni per notifica mail/sms.
-end
+BEGIN        
+SET NOCOUNT ON;
+
+DECLARE @NombreDocumento VARCHAR(MAX) = (SELECT INSERTED.NombreDocumento 
+                                         FROM INSERTED 
+                                         WHERE INSERTED.IdCircuito = 53 
+                                         AND INSERTED.IdTipoDoc = 45 
+                                         AND INSERTED.Gestor = 'Gest1')
+
+IF @NombreDocumento IS NOT NULL
+BEGIN
+
+    EXEC msdb.dbo.sp_send_dbmail
+      @recipients = 'rsg@gmail.com', 
+      @subject = 'New requeriment', 
+      @body = 'It''s a new requeriment: ' + @NombreDocumento;
+END
+END
+GO
 //
-delimiter ;
+delimiter ;*/
+select * from Utente;
 
 
 
