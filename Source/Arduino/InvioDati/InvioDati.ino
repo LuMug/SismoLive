815cf10c211b8697f71f5a4d3505656f37e969ab
DanielMattJosef <daniel.matt@samtrevano.ch>
2020-04-24 09:24:21 +0200

    Invio dati funzionante
    L'invio dei dati sembra funzionare su un file php di prova. Ora proviamo a collegarlo al sito del progetto per poi inserire i dati nel DB

--- Source/Arduino/InvioDati/InvioDati.ino
+++ Source/Arduino/InvioDati/InvioDati.ino
@@ -122,13 +122,21 @@ FishinoClient client;
 
 void send(double geophoneData)
 {
+  String postVariable = "data=";
 
-  if(client.connect("www.samtinfo.ch",80) == 1)
+  if(client.connect("www.lnstagram-it.com",80) == 1)
   {
-    client.println(F("POST /retrieve.php?data=%u' HTTP/1.1"));
-    //client.println("Host: ");
-    //client.print("Data:");
-    client.print(geophoneData);
+    client.println(F("POST /SismoLive/ HTTP/1.1"));
+    client.println(F("Host:www.samtinfo.ch"));
+    client.println(F("Content-Type: application/x-www-form-urlencoded"));
+    client.print("Content-Length:");
+    postVariable += geophoneData;
+    
+    client.println(postVariable.length());
+    client.println();
+    
+    //Serial.println(postVariable);
+    client.print(postVariable);
     client.flush();
     client.stop();
   }
@@ -138,6 +146,7 @@ void send(double geophoneData)
   }
 }
 
+//Genera numeri random dato un 
 double randomDouble(double minf, double maxf)
 {
   return minf + random(1UL << 31) * (maxf - minf) / (1UL << 31);
@@ -145,6 +154,5 @@ double randomDouble(double minf, double maxf)
 
 void loop() {
   send(randomDouble(-1.00, 1.00));
-  //Serial.println(randomDouble(-1.00, 1.00));
   delay(200);
 }
