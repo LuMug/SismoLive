e8e275b43b372130338d99942c04759f1385d148
Matthias Iannarella <54986050+MatthiasIannarella@users.noreply.github.com>
2020-05-06 11:08:45 +0200

    Aggiunta la possibilita di configurare la soglia intermedia

--- Source/Sito/php/configurazione.php
+++ Source/Sito/php/configurazione.php
@@ -13,17 +13,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         $min = trim($_POST["min"]);
     }
 
+    if (!(empty(trim($_POST["intermedia"]))))
+    {
+        $intermedia = trim($_POST["intermedia"]);
+    }
+
     if (!(empty(trim($_POST["critica"]))))
     {
         $critica = trim($_POST["critica"]);
     }
 
     $min = filter_input(INPUT_POST, 'min', FILTER_DEFAULT);
+    $intermedia = filter_input(INPUT_POST, 'intermedia', FILTER_DEFAULT);
     $critica = filter_input(INPUT_POST, 'critica', FILTER_DEFAULT);
-    if($min > $critica){
-      $_SESSION['error'] = "La soglia minima deve essere minore di quella critica";
+    if($min > $intermedia){
+      $_SESSION['error'] = "La soglia minima deve essere più piccola della soglia intermedia";
+
+    }else if($min > $critica){
+      $_SESSION['error'] = "La soglia minima deve essere più piccola della soglia critica";
+    }else if($intermedia > $critica){
+  $_SESSION['error'] = "La soglia intermedia deve essere più piccola della soglia critica";
     }else{
-      $terremoti = "UPDATE Configurazione SET soglia_minima = '$min', soglia_critica = '$critica'";
+      $terremoti = "UPDATE Configurazione SET soglia_minima = '$min', soglia_intermedia = '$intermedia', soglia_critica = '$critica' ";
       $result = $link->query($terremoti);
     }
     header("location: ../html/configurazione.php");
